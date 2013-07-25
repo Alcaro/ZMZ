8e797c422154af8e8f646a6ac7cdb8cde31347eb
Alcaro <floating@muncher.se>
2013-07-25 15:34:02 +0200

    Update version

--- ui.c
+++ ui.c
@@ -19,7 +19,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-char *ZVERSION = "ZMZ1.07";
+char *ZVERSION = "ZMZ1.08";
 
 
 #ifdef __UNIXSDL__
