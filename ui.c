09828c8c2c5c029de431f26a6ba15379a5c49c90
Alcaro <floating@muncher.se>
2013-07-27 09:37:46 +0200

    Bump version

--- ui.c
+++ ui.c
@@ -19,7 +19,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-char *ZVERSION = "ZMZ1.08";
+char *ZVERSION = "ZMZ1.09";
 
 
 #ifdef __UNIXSDL__
